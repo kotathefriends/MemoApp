8b992fc7f7964f21271a35f95bcaf7b88e3d9455
kotathefriends <5070kouta@gmail.com>
2024-01-30 15:05:17 +0900

    Initial Commit

---
+++ MemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoApp
+//
+//  Created by KOTA TAKAHASHI on 2024/01/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoApp/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp
+//
+//  Created by KOTA TAKAHASHI on 2024/01/30.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
