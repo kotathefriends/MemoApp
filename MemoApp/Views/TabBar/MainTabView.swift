378042d96ecb4bad1f21efc04da292473065a857
kotathefriends <5070kouta@gmail.com>
2024-02-01 23:45:46 +0900

    TabBarを押した時に .medium のハプティックフィードバックを返す処理の実装

--- MemoApp/Views/TabBar/MainTabView.swift
+++ MemoApp/Views/TabBar/MainTabView.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 
 struct MainTabView: View {
+    
+    @State private var selectioin = 1
+    
+//    ハプティックフィードバックを生成するための関数
+    func generateHapticFeedback() {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.prepare()
+        generator.impactOccurred()
+    }
+    
     var body: some View {
         TabView {
             HomeView() // ホームビューのタブ
                 .tabItem {
                     Label("ホーム", systemImage: "house")
                 }
+                .tag(1)
+                .onAppear {
+                    if selectioin == 1 {
+                        generateHapticFeedback()
+                    }
+                }
 
             FeedView() // フィードビューのタブ
                 .tabItem {
                     Label("フィード", systemImage: "list.bullet")
+                }
+                .tag(2)
+                .onAppear {
+                    if selectioin == 2 {
+                        generateHapticFeedback()
+                    }
                 }
         }
     }
