49e9c6838152613fd2365ee62ae80378989352d2
kotathefriends <5070kouta@gmail.com>
2024-03-12 16:40:03 +0900

    InviteFriendsCardView を sheet で表示させるように実装した

--- MemoApp/Friends/InviteFriendsCardView.swift
+++ MemoApp/Friends/InviteFriendsCardView.swift
@@ -21,34 +21,48 @@ extension Color {
 }
 
 struct InviteFriendsCardView: View {
+    
+    @State private var isShowInviteFriendsView = false
+    
     var body: some View {
         
-        
-        HStack(spacing: 8) {
-            Image("emmacorrin-2")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 40, height: 40)
-            .clipShape(Circle())
-            
-            
-            VStack(alignment: .leading) {
-                Text("Invite a friend")
-                    .fontWeight(.semibold)
+        Button(action: {
+            self.isShowInviteFriendsView =  true
+        }) {
+            HStack(spacing: 8) {
+                Image("emmacorrin-2")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
                 
-                Text("@emma_amme")
-                    .font(.system(size: 16))
+                
+                VStack(alignment: .leading) {
+                    Text("Invite a friend")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black)
+                    
+                    Text("@emma_amme")
+                        .font(.system(size: 16))
+                        .foregroundColor(.gray)
+
+                    
+                }
+                Spacer()
+                
+                Image(systemName: "square.and.arrow.up")
+                    .foregroundColor(.black)
 
             }
-            Spacer()
+            .padding(.horizontal, 16)
+            .frame(width: 350, height: 72)
+            .background(Color(hex: 0xF4EFD9))
+            .cornerRadius(14)
             
-            Image(systemName: "square.and.arrow.up")
         }
-        .padding(.horizontal, 16)
-        .frame(width: 350, height: 72)
-        .background(Color(hex: 0xF4EFD9))
-        .cornerRadius(14)
-
+        .sheet(isPresented: $isShowInviteFriendsView) {
+            InviteFriendsView()
+        }
     }
 }
 
---
+++ MemoApp/Friends/InviteFriendsView.swift
@@ -0,0 +1,18 @@
+//
+//  InviteFriendsView.swift
+//  MemoApp
+//
+//  Created by KOTA TAKAHASHI on 2024/03/12.
+//
+
+import SwiftUI
+
+struct InviteFriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InviteFriendsView()
+}
