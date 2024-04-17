2d245a7cc5fd8cfca5fa70493babf5988300d50b
kotathefriends <5070kouta@gmail.com>
2024-04-17 13:35:56 +0900

    DetailMwmoView 作るど

---
+++ MemoApp/Views/Memo/DetailMemoView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailMemoView.swift
+//  MemoApp
+//
+//  Created by KOTA TAKAHASHI on 2024/04/17.
+//
+
+import SwiftUI
+
+struct DetailMemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailMemoView()
+}
